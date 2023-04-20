a8fb92b4c9ccbdc4e95c935e63c38eb2a9f80d92
coolIlive <galilee155@naver.com>
2023-04-20 12:23:04 +0900

    Feat: 회원가입 성공시 main으로 이동
    - form 태그 action에 경로 추가함
    - main페이지 작업중

--- backend/router/index.js
+++ backend/router/index.js
@@ -4,8 +4,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("./path");
 
-//일단 기본 화면을 main이 아니라 로그인 화면으로 해놨어요.
-router.get("/",path.login);
+router.get("/",path.home);
 
 router.get("/login",path.login);
 
