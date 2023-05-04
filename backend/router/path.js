1657a62d0f4f1252268e85b131759dd16d1a184e
coolIlive <galilee155@naver.com>
2023-05-04 18:41:37 +0900

    Design: share페이지의 게시물 부분 추가
    - 각 게시물을 card형식으로 보임
    - 각 게시물에 들어가는 내용 : 프로필,닉네임,이미지,아이콘(좋아요,다운로드,댓글), 게시글

--- backend/router/path.js
+++ backend/router/path.js
@@ -18,10 +18,15 @@ const generate = (req, res) => {
     res.sendFile(path.join(__dirname,'../../frontend/src/html/generate.html'));
 };
 
+const share = (req, res) => {
+    res.sendFile(path.join(__dirname,'../../frontend/src/html/share.html'));
+};
+
 // 컨트롤러 모듈 내보내기
 module.exports = {
     main,
     login,
     signup,
     generate,
+    share,
 };
