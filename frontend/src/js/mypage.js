a71ed5d38a41ab6feaa9c89e2288838609dd1a3a
coolIlive <galilee155@naver.com>
2023-05-15 18:50:52 +0900

    Feat: mypage 카테고리 접고 펼치기 기능 추가

---
+++ frontend/src/js/mypage.js
@@ -0,0 +1,17 @@
+window.onload=()=>{
+  document.querySelector('.menu-trigger').onclick = ()=>{
+    var burger = $('.menu-trigger');
+    var category_btn = $('.category_group');
+    // var category_btn = document.getElementsByClassName('category_group');
+
+    burger.each(function(index) {
+      $(this).toggleClass('active-8');
+      if (category_btn.css('display') == 'none') {
+        category_btn.css("display","flex");
+      } else {
+        category_btn.css("display","none");
+      }
+    });
+
+  }
+}
