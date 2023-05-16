307d4bdc3aafd70c918f06e5437ac97e07adafbd
coolIlive <galilee155@naver.com>
2023-05-16 19:22:29 +0900

    Fix: mypage 화면조정시 햄버거버튼 및 카테고리 오류 고침
    - 작은 화면에서 카테고리 접으면 화면이 클때도 안보이게 됨
    -> css가 아니라 js를 사용하여 해결

--- frontend/src/js/mypage.js
+++ frontend/src/js/mypage.js
@@ -8,12 +8,26 @@ window.onload=()=>{
       $(this).toggleClass('active-8');
       if (category_btn.css('display') == 'none') {
         category_btn.css("display","flex");
-        // category_btn.css("visibility","visible");
-        // category_btn.css("width","visible");
       } else {
         category_btn.css("display","none");
       }
     });
+  };
+  
+  window.onresize = function(event) {
+    var innerWidth = window.innerWidth;
+    var burger = $('.menu-trigger');
+    var category_btn = $('.category_group');
 
-  }
-}
+    if (innerWidth < '1032') {
+      burger.each(function(index) {
+        if ($(this).hasClass('active-8')) {
+          $(this).toggleClass('active-8');
+        }
+      });
+      category_btn.css("display","none");
+    } else {
+      category_btn.css("display","flex");
+    };
+  };
+};
