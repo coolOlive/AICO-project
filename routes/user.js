7900a540006a61b358ca9ad4c35e4c036f1f171c
Jiwon Ryu <ryujiwon11@naver.com>
2023-05-31 14:21:48 +0900

    로그인 name 변경

--- routes/user.js
+++ routes/user.js
@@ -33,16 +33,16 @@ const router = express.Router();
 //     next(error);
 //   }
 // });
-// router.post('/profile', async(req, res, next) => {
-//   try{
-//     await User.update({ nick: req.body.nick }, {
-//       where: { id: req.user.id},
-//     });
-//     res.redirect('/profile');
-//   }catch (error) {
-//     console.error(error);
-//     next(error);
-//   }
-// });
+router.post('/profile', async(req, res, next) => {
+  try{
+    await User.update({ nick: req.body.nick }, {
+      where: { id: req.user.id},
+    });
+    res.redirect('/profile');
+  }catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
