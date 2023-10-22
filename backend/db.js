b7f713c3936c006364663b884fc7aa42d53f7b99
racoi <87438305+racoi@users.noreply.github.com>
2023-10-22 16:42:54 +0900

    s3 unique link, db conn
    s3 unique link, db conn

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-// var mysql = require('mysql2');
-// var db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '0000',
-//     database: 'aico_db'
-// });
-// db.connect();
+var mysql = require('mysql2');
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '0000',
+    database: 'aico_db'
+});
+db.connect();
 
-// module.exports = db;
+module.exports = db;
