03860062ee12e10a57fb00b0ee780c7c6d2cda6c
coolIlive <galilee155@naver.com>
2023-11-27 13:18:38 +0900

    feat: 공유페이지 팝업창에 생성 문구 출력

--- frontend/src/js/post.js
+++ frontend/src/js/post.js
@@ -7,11 +7,12 @@ function showPopup(imgsrc, user, content) {
   let popup = document.getElementById("popup");
   let clickedImg = document.getElementById("post_img");
   let popupUser = document.getElementById("post_popup_userid");
+  let cardTxt = document.getElementById("popupTxt");
   alert(user);
   alert(content);
 
   popupUser.innerHTML = user;
-
+  cardTxt.innerHTML = content;
   clickedImg.src = imgsrc;
   popup.style.display = "block";
 }
