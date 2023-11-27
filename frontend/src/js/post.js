448ed89c384ea4d78ffcb4e58d7f9120c766a004
coolIlive <galilee155@naver.com>
2023-11-27 12:51:03 +0900

    feat: 공유페이지 팝업에 이미지 띄우기

--- frontend/src/js/post.js
+++ frontend/src/js/post.js
@@ -3,7 +3,10 @@ function closePopup() {
   popup.style.display = "none";
 }
 
-function showPopup() {
+function showPopup(imgsrc) {
   let popup = document.getElementById("popup");
+  let clicked_img = document.getElementById("post_img");
+
+  clicked_img.src = imgsrc;
   popup.style.display = "block";
 }
